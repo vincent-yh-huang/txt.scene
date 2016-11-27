ed4036815f8d644e47e763b107a324586db5c9a9
Vincent Huang <vincent.yh.huang@gmail.com>
2016-11-26 17:29:15 -0800

    refactored again.

--- src/io/github/vyhhuang/parser/TType.scala
+++ src/io/github/vyhhuang/parser/TType.scala
@@ -1,13 +1,14 @@
-package io.github.username.vyhhuang.token
+package io.github.vyhhuang.parser
+
 sealed abstract class TType() {
-    type T
-    val tVal: T
-    val s: String
-    override def toString = s + tVal
+  type T
+  val tVal: T
+  val s: String
+  override def toString = s + tVal
 }
 case class TNum(tVal: Int) extends TType {type T=Int;val s="#"}
 case class TKWord(tVal: String) extends TType {type T=String;val s="!"}
 case class TWord(tVal: String) extends TType {type T=String;val s="\""}
 case class TScene(tVal: String) extends TType {type T=String;val s="##"}
 case class TParen(tVal: String) extends TType {type T=String;val s="\""}
-case class TNLine(tVal: String = "\\n") extends TType {type T=String;val s=""}
+case class TNLine(tVal: String="\\n") extends TType {type T=String;val s=""}
