1be401d6c8c53ea17692140874bc1056d81f7d42
Vincent Huang <vincent.yh.huang@gmail.com>
2016-11-26 16:48:22 -0800

    moved to Eclipse project

---
+++ src/TType.scala
@@ -0,0 +1,12 @@
+sealed abstract class TType() {
+    type T
+    val tVal: T
+    val s: String
+    override def toString = s + tVal
+}
+case class TNum(tVal: Int) extends TType {type T=Int;val s="#"}
+case class TKWord(tVal: String) extends TType {type T=String;val s="!"}
+case class TWord(tVal: String) extends TType {type T=String;val s="\""}
+case class TScene(tVal: String) extends TType {type T=String;val s="##"}
+case class TParen(tVal: String) extends TType {type T=String;val s="\""}
+case class TNLine(tVal: String = "\\n") extends TType {type T=String;val s=""}
